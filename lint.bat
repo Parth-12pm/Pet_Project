dd47d5f195e91a293e753761f05589e8000d8e7a
Parth-12pm <parthsmahadik12027@gmail.com>
2025-09-07 00:07:24 +0530

    connected the database

---
+++ lint.bat
@@ -0,0 +1,2 @@
+@echo off
+pre-commit run --all
--- src/main.py
+++ src/main.py
@@ -1,7 +1,26 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from .api.views import router as api_router
+from .db.util import close_db_client, get_db_client
+from .pet_project.settings import settings
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    # set up mongodb
+    client = get_db_client()
+    app.state.mongodb = client.get_database(settings.MONGO_DB_NAME)
+    yield
+    close_db_client()
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(api_router)
+
 
 @app.get("/")
-async def root():
+async def read_root() -> dict[str, str]:
     return {"message": "Hello World this is my first FastAPI application"}
